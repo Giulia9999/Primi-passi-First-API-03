16ef4c9cddaa5c080e1ef5127b8ea0b57c4fc846
Giulia <giuliac35@gmail.com>
2023-04-12 19:13:15 +0200

    exercise completed

--- src/main/java/it/develhope/primi_passi_first/API_03/StringController.java
+++ src/main/java/it/develhope/primi_passi_first/API_03/StringController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class StringController {
     @GetMapping("/strings")
     public String concateStrings(@RequestParam String string1, @RequestParam(required = false) String string2){
+        if(string2 == null){
+            return string1;
+        }
         return string1 + " " + string2;
     }
 }
